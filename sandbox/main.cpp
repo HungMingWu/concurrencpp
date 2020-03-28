5744d39d7bf8dfdd3e4aa612d3a07a71526a4aaf
David Haim <dhayim1@gmail.com>
2020-03-28 13:18:06 +0300

    * sandbox/main revert :)

--- sandbox/main.cpp
+++ sandbox/main.cpp
@@ -2,28 +2,11 @@
 
 #include <iostream>
 
-#include <Windows.h>
-
 int main() {
-
-	concurrencpp::details::single_worker_thread worker("");
-
-	std::atomic<std::chrono::high_resolution_clock::time_point> start{ std::chrono::high_resolution_clock::now() };
-	for (size_t i = 0; i < 10'024; i++) {
-		worker.enqueue([] {});
-	}
-
-	worker.enqueue([s = start.load()]{
-		const auto now = std::chrono::high_resolution_clock::now();
-		auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(now - s);
-
-		std::cout << ms.count() << std::endl;
-		});
-
-	worker.wait_all();
-	std::cout << "done waiting" << std::endl;
-	std::getchar();
-
+	auto runtime = concurrencpp::make_runtime();
+	auto result = runtime->thread_pool_executor()->submit([] {
+		std::cout << "hello world" << std::endl;
+	});
 
 	return 0;
 }
